9d3fd13ef7c14287a18ccd1ccf6833184e949876
fabrojonathan <76507512+fabrojonathan@users.noreply.github.com>
2023-03-19 18:09:07 -0300

    Add body ConversorMasa

--- src/Conversor/ConversorMasa.java
+++ src/Conversor/ConversorMasa.java
@@ -1,5 +1,24 @@
 package Conversor;
 
-public class ConversorMasa {
+import Data.Masa;
+
+public class ConversorMasa implements Conversor{
+
+	@Override
+	public double Convertir(int pUnidadInicial, int pUnidadFinal, double pDigito) {
+		Masa vUnidadInicial = Masa.values()[pUnidadInicial];
+		Masa vUnidadFinal = Masa.values()[pUnidadFinal];
+		double difUnidades;
+		double resultConversion = 0;
+		
+		if (!(vUnidadInicial == vUnidadFinal)) {
+			difUnidades = vUnidadInicial.compareTo(vUnidadFinal);
+			resultConversion = pDigito * Math.pow(10, (difUnidades));
+		}
+		
+		//System.out.println(resultConversion+ " " + Masa.values()[pUnidadFinal].name() + "s");
+
+		return resultConversion;
+	}
 
 }
