6833da6ec5ddc9f8ab8db7b1a867b23c68c05f75
fabrojonathan <76507512+fabrojonathan@users.noreply.github.com>
2023-03-19 17:40:29 -0300

    Change bodyConversorLongitud

--- src/Conversor/ConversorLongitud.java
+++ src/Conversor/ConversorLongitud.java
@@ -1,5 +1,22 @@
 package Conversor;
 
-public class ConversorLongitud {
+import Data.Longitud;
 
+public class ConversorLongitud implements Conversor{
+
+	@Override
+	public double Convertir(int pUnidadInicial, int pUnidadFinal, double pDigito) {
+		Longitud vUnidadInicial = Longitud.values()[pUnidadInicial];
+		Longitud vUnidadFinal = Longitud.values()[pUnidadFinal];
+		double difUnidades;
+		double resultConversion = 0;
+		
+		if (!(vUnidadInicial == vUnidadFinal)) {
+			difUnidades = vUnidadInicial.compareTo(vUnidadFinal);
+			resultConversion = pDigito * Math.pow(10, (difUnidades));
+		}
+		//System.out.println((int)resultConversion+ " " + Longitud.values()[pUnidadFinal].name() + "s");
+		return resultConversion;
+	}
+	
 }
