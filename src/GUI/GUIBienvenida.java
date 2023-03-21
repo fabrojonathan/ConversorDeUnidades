28199fe204d87535ad91df8c5d44bdc0d80e2684
fabrojonathan <76507512+fabrojonathan@users.noreply.github.com>
2023-03-21 20:49:45 -0300

    Change design Interfaz

--- src/GUI/GUIBienvenida.java
+++ src/GUI/GUIBienvenida.java
@@ -5,6 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Font;
 
 public class GUIBienvenida extends JFrame {
 
@@ -30,12 +34,32 @@ public class GUIBienvenida extends JFrame {
 	 * Create the frame.
 	 */
 	public GUIBienvenida() {
+		setLocationByPlatform(true);
+		setUndecorated(true);
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 640, 350);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("By Fabro Jonathan - Alura - OneOracle");
+		lblNewLabel_1.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 14));
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setBounds(10, 318, 251, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Conversor de Unidades");
+		lblNewLabel_1_1.setForeground(Color.WHITE);
+		lblNewLabel_1_1.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 14));
+		lblNewLabel_1_1.setBounds(10, 11, 150, 14);
+		contentPane.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\jonhf\\eclipse-workspace\\ConversorDeUnidades\\src\\Resources\\wallpaper-756032_640.png"));
+		lblNewLabel.setBounds(0, 0, 640, 350);
+		contentPane.add(lblNewLabel);
 	}
-
 }
