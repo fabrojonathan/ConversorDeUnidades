d34dd6aa52f255bb582adec04da2c704c70dc893
fabrojonathan <76507512+fabrojonathan@users.noreply.github.com>
2023-03-21 20:48:37 -0300

    Change show GUIs

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
+import GUI.GUIBienvenida;
 import GUI.GUIPrincipal;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		GUIPrincipal newGUI = new GUIPrincipal();
-		newGUI.setVisible(true);
-
+		GUIBienvenida nBienvenida = new GUIBienvenida();
+		nBienvenida.setLocationRelativeTo(null);
+		nBienvenida.setVisible(true);
+		GUIPrincipal nGUIPrincipal = new GUIPrincipal();
+		nGUIPrincipal.setLocationRelativeTo(null);
+		
+		Timer timer = new Timer(4000,new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                nBienvenida.setVisible(false);
+                nGUIPrincipal.setVisible(true);
+        }});
+		
+		timer.start();
 	}
 
 }
