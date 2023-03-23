b514362819af11746a41bc1a5a9d02fec35eb25e
fabrojonathan <76507512+fabrojonathan@users.noreply.github.com>
2023-03-22 23:25:19 -0300

    Add design and functions

--- src/GUI/PanelDivisas.java
+++ src/GUI/PanelDivisas.java
@@ -2,58 +2,111 @@ package GUI;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.SoftBevelBorder;
+
+import Conversor.ConversorDivisas;
+
+import javax.swing.border.CompoundBorder;
+import javax.swing.SwingConstants;
+import javax.swing.border.MatteBorder;
+import java.awt.Component;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PanelDivisas extends JPanel {
-	private JTextField textField;
+	private JTextField txtMonto;
 
 	/**
 	 * Create the panel.
 	 */
 	public PanelDivisas() {
+		
 		setLayout(null);
+		
+		JPanel panelResult = new JPanel();
+		panelResult.setLayout(null);
+		panelResult.setBorder(null);
+		panelResult.setBackground(new Color(160, 172, 189));
+		panelResult.setBounds(134, 255, 256, 40);
+		add(panelResult);
+		
+		JLabel txtResult = new JLabel("--");
+		txtResult.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		txtResult.setBounds(0, 0, 256, 40);
+		panelResult.add(txtResult);
+		txtResult.setHorizontalAlignment(SwingConstants.CENTER);
+		txtResult.setFont(new Font("Tahoma", Font.ITALIC, 18));
 		
 		JLabel lblNewLabel = new JLabel("Ingrese el monto");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel.setBounds(33, 57, 193, 14);
+		lblNewLabel.setBounds(70, 64, 137, 14);
 		add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		textField.setColumns(10);
-		textField.setBounds(236, 53, 181, 23);
-		add(textField);
+		txtMonto = new JTextField();
+		txtMonto.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtMonto.setColumns(10);
+		txtMonto.setBounds(217, 60, 248, 23);
+		add(txtMonto);
 		
 		JComboBox cBoxDivisaBase = new JComboBox();
-		cBoxDivisaBase.setBounds(236, 109, 181, 22);
+		cBoxDivisaBase.setModel(new DefaultComboBoxModel(new String[] {"Pesos", "Dolar", "DolarAustraliano", "Euro", "Libra", "Real", "Yen", "PesoMexicano", "PesoUruguayo", "Yuan"}));
+		cBoxDivisaBase.setBounds(217, 106, 248, 22);
 		add(cBoxDivisaBase);
 		
 		JComboBox cBoxDivisaFinal = new JComboBox();
-		cBoxDivisaFinal.setBounds(236, 161, 181, 22);
+		cBoxDivisaFinal.setModel(new DefaultComboBoxModel(new String[] {"Pesos", "Dolar", "DolarAustraliano", "Euro", "Libra", "Real", "Yen", "PesoMexicano", "PesoUruguayo", "Yuan"}));
+		cBoxDivisaFinal.setBounds(217, 153, 248, 22);
 		add(cBoxDivisaFinal);
 		
 		JLabel lblNewLabel_2 = new JLabel("Divisa final");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_2.setBounds(33, 163, 193, 14);
+		lblNewLabel_2.setBounds(70, 155, 137, 14);
 		add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_1 = new JLabel("Divisa base");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_1.setBounds(33, 111, 193, 14);
+		lblNewLabel_1.setBounds(70, 108, 137, 14);
 		add(lblNewLabel_1);
 		
-		JButton btnNewButton = new JButton("Convertir");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnNewButton.setBounds(222, 237, 140, 25);
-		add(btnNewButton);
+		JButton btnConvertir = new JButton("Convertir");
+		btnConvertir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Double monto = Double.parseDouble(txtMonto.getText());
+				int indexBase = cBoxDivisaBase.getSelectedIndex();
+				int indexFinal = cBoxDivisaFinal.getSelectedIndex();
+				
+				ConversorDivisas nConversor = new ConversorDivisas();
+				Double resultadoConversion = nConversor.Convertir(indexBase, indexFinal, monto);
+				
+				txtResult.setText(cBoxDivisaFinal.getSelectedItem().toString() + " " + resultadoConversion.toString());
+			}
+		});
+		btnConvertir.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnConvertir.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnConvertir.setBounds(325, 197, 140, 25);
+		add(btnConvertir);
 		
-		JButton btnSalir = new JButton("Salir");
-		btnSalir.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnSalir.setBounds(372, 237, 140, 25);
-		add(btnSalir);
+		JPanel panel = new JPanel();
+		panel.setBorder(new CompoundBorder(null, new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
+		panel.setBackground(new Color(241, 250, 255));
+		panel.setBounds(10, 11, 216, 23);
+		add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_3 = new JLabel("Conversor de Divisas");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 12));
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_3.setBounds(10, 0, 206, 23);
+		panel.add(lblNewLabel_3);
 
 	}
 }
