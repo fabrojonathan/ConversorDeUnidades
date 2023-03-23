52646052f0990c6449fb27d337f5b8b9765e4e5d
fabrojonathan <76507512+fabrojonathan@users.noreply.github.com>
2023-03-23 20:38:18 -0300

    Add desing

--- src/GUI/PanelHome.java
+++ src/GUI/PanelHome.java
@@ -3,6 +3,9 @@ package GUI;
 import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JPasswordField;
+import javax.swing.JTextPane;
 
 public class PanelHome extends JPanel {
 
@@ -13,9 +16,21 @@ public class PanelHome extends JPanel {
 		setBackground(new Color(241, 250, 255));
 		setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Bienvenido!!");
-		lblNewLabel.setBounds(246, 121, 194, 22);
+		JLabel lblNewLabel = new JLabel("Bienvenido!");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
+		lblNewLabel.setBounds(10, 11, 194, 22);
 		add(lblNewLabel);
+		
+		JTextPane txtpnEstaAplicacinDe = new JTextPane();
+		txtpnEstaAplicacinDe.setFont(new Font("Tahoma", Font.ITALIC, 14));
+		txtpnEstaAplicacinDe.setText("Esta aplicación le facilitara la vida.\r\nCon pocos clicks podra realizar cualquier tipo de conversión.\r\nIngrese la medida o monto, seleccione la unidad base y la unidad final, y la aplicación\r\nle mostrara el resultado!\r\n\r\n\r\n\t\t\t\t\t   \t\t   Desarrollada por: Fabro Jonathan\r\n\t\t  De la mano de: OneOracle - Alura");
+		txtpnEstaAplicacinDe.setBounds(143, 125, 384, 184);
+		add(txtpnEstaAplicacinDe);
 
+	}
+	private static class __Tmp {
+		private static void __tmp() {
+			  javax.swing.JPanel __wbp_panel = new javax.swing.JPanel();
+		}
 	}
 }
